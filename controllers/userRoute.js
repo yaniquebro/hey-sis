9c11a60c83de4e398c7db8814e4883095f99fe0f
clarkera <rclarke9941@gmail.com>
2024-06-04 21:04:24 -0400

    user route added handlebars dependency'

---
+++ controllers/userRoute.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { User } = require('../models');
+
+
+router.get('/', async (req, res) => {
+  try {
+    
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
+    
+    const users = userData.map((project) => project.get({ plain: true }));
+    
+    res.render('homepage', { users });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
