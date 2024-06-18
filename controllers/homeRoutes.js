603aaaec6d7cde1342a4f96bf81814b56b82a8a3
clarkera <rclarke9941@gmail.com>
2024-06-18 18:14:08 -0400

    syntax

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const  User  = require('../models/User');
+const  User  = require('../models/user');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
