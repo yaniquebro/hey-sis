5074b06ac166af27d73f4a168747e44a62d0340b
clarkera <rclarke9941@gmail.com>
2024-06-18 18:07:17 -0400

    url

--- config/connections.js
+++ config/connections.js
@@ -3,8 +3,8 @@ require("dotenv").config()
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL) 
+if (process.env.DB_URL) {
+    sequelize = new Sequelize(process.env.DB_URL) 
 } else {
     sequelize = new Sequelize(
         process.env.DB_NAME,
